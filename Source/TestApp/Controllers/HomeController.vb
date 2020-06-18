f1ae5b5ee809bd1b7e82d6c126e646390d32a954
Fernando Forero <fernandoforero@filevine.com>
2020-06-18 15:35:20 -0600

    One more try

--- Source/TestApp/Controllers/HomeController.vb
+++ Source/TestApp/Controllers/HomeController.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        ViewData("Title") = "Home Page 1"
+        ViewData("Title") = "Home Page"
 
         Return View()
     End Function
