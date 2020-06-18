7f455df9ec034eb6e6f0a02b5c3ad649e83f7df5
Fernando Forero <fernandoforero@filevine.com>
2020-06-18 16:38:13 -0600

    This one should not trigger a tag-filtered build

--- Source/TestApp/Controllers/HomeController.vb
+++ Source/TestApp/Controllers/HomeController.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        ViewData("Title") = "Home Page"
+        ViewData("Title") = "Home Pages"
 
         Return View()
     End Function
